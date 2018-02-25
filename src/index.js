0d6c51d12e0d55743a8080633a31e5c665f329f1
Mark Birbeck <mark.birbeck@gmail.com>
2018-02-25 22:30:47 +0000

    [#2] turn spawning of Mocha into a function

--- src/index.js
+++ src/index.js
@@ -5,11 +5,20 @@ const { spawn } = require('child_process');
  * Run Mocha:
  */
 
-spawn('mocha', [
-    '--inspect',         // Enable Node inspector
-    '--recursive',       // Run any tests found in the main directory and subdirectories
-    '--reporter', 'min', // Minimal reporting output
-    '--watch'            // Watch for file changes
-  ],
-  { stdio: 'inherit' }
-);
+const runMocha = () => {
+  spawn('mocha', [
+      '--inspect',         // Enable Node inspector
+      '--recursive',       // Run any tests found in the main directory and subdirectories
+      '--reporter', 'min', // Minimal reporting output
+      '--watch'            // Watch for file changes
+    ],
+    { stdio: 'inherit' }
+  );
+};
+
+
+/**
+ * Run Mocha once on first run:
+ */
+
+runMocha();
